0f6e9d92ea1ec3fb56c3172e4cb4f40d0077d9e8
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-20 13:37:34 +0100

    [UPD]add invitedMail array in script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//inizializzo array con le mail invitate alla festa
+const invitedMail = ['salvatore@gmail.com', 'alessio@hotmail.com', 'marco@libero.it', 'katia@virgilio.it'];
+
+//chiedo all'utente la sua mail
+let userMail = prompt('Inserisci la tua mail');
+
