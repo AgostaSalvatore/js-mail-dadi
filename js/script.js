96b184df74d9710d675ace634a00dabc667243c1
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-20 13:42:33 +0100

    [UPD] done mail check

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ const invitedMail = ['salvatore@gmail.com', 'alessio@hotmail.com', 'marco@libero
 //chiedo all'utente la sua mail
 let userMail = prompt('Inserisci la tua mail');
 
+//inizializzo la varabile di confronto
+let checkMail;
+
+//ciclo for
+for(let i = 0; !checkMail && i<invitedMail.length; i++){
+    if(invitedMail[i] == userMail){
+        checkMail = true;
+    }
+}
+
+if(checkMail){
+    console.log(`La tua mail e' presente tra gli invitati, complimenti`)
+}
+else{
+    console.log(`La tua mail non e' presente tra gli invitati, mi dispiace`)
+}
